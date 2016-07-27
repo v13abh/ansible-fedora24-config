04647c94d0274be94011cd057dbc96de7b56930c
Chris Cone <chris.cone@ou.edu>
2016-07-27 08:50:06 -0500

    Updated install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # Script to deploy Fedora 24 as DevOps workstation
 
-# sudo dnf update -y
+sudo dnf update -y
 
 sudo dnf install python2-dnf -y
 
 sudo dnf install ansible -y
 
+cd ~/ansible-fedora24-config
 
+ansible-playbook playbook.yml -K
