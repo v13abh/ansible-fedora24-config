69b4c41527400c4dbe91856c1bba4573f7bd2713
Chris Cone <chris.cone@ou.edu>
2016-07-27 13:43:05 -0500

    Troubleshooting install script

--- install3.sh
+++ install3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+read -p "We are now ready to continue the deployment of Fedora as a DevOps workstation. Press [ENTER] to continue..."
+
 sudo dnf install python2-dnf -y
 
 sudo dnf install ansible -y                                                                                                      
